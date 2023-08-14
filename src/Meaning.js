2e5d33053e808502817f56261971cc560d2cdb2f
noriin98 <125356694+noriin98@users.noreply.github.com>
2023-08-14 20:55:52 +0200

    m

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Synonyms from "./Synonyms"; 
 
 export default function Meaning(props) {
+    
 return (
     <div className="Meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
@@ -9,13 +11,15 @@ return (
             return (
                 <div key={index}>
                 <p>
-                {definition.definition}
+                <strong>Definition:</strong> {definition.definition}
                 <br />
+                <strong>Example:</strong>
                 <em>{definition.example}</em>
+                <Synonyms synonyms={definition.synonyms} />
                 </p>
                 </div>
             );
         })}
     </div>
-) 
+); 
 }
